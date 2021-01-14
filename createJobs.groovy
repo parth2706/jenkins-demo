8dffd01ebea6505775b8ca8010b59eda9f8990ca
Parth Mehta <mehta.parth2706@gmail.com>
2021-01-13 18:25:12 -0600

    Corrected the git link to point to the spring boot application

--- createJobs.groovy
+++ createJobs.groovy
@@ -13,7 +13,7 @@ pipelineJob('theme-park-job') {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/parth2706/jenkins-demo.git'
+                        url 'https://github.com/parth2706/spring-boot-api-example.git'
                     }
                     branch 'master'
                 }
